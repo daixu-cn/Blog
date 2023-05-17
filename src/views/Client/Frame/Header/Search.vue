8da58af7cc285d2625361fa7305fe4682e1f072e
daixu <daixu.cn@outlook.com>
2023-05-17 20:54:16 +0800

    feat(components): 搜索模块

---
+++ src/views/Client/Frame/Header/Search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="Search" class="dropdown-container">
+    <Icon class="icon-search" @click="searchHandler" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits(["open"])
+function searchHandler() {
+  const HeaderEL = document.querySelector("#Header") as HTMLElement
+  HeaderEL.setAttribute("class", "hidden")
+
+  function listener(event: TransitionEvent) {
+    if (event.target === HeaderEL) {
+      emit("open")
+      HeaderEL.removeEventListener("transitionend", listener)
+    }
+  }
+  HeaderEL.addEventListener("transitionend", listener, false)
+}
+</script>
---
+++ src/views/Client/Frame/Header/SearchModal.vue
@@ -0,0 +1,262 @@
+<template>
+  <div id="SearchModal" :class="{ enter: isSearch }" @click="maskClick">
+    <div class="input">
+      <i-ep-search class="searchIcon" />
+      <el-input
+        ref="input"
+        v-model="keyword"
+        placeholder="文章标题"
+        @input="getList"
+      />
+      <i-ep-close class="clearIcon" @click="keyword = ''" />
+    </div>
+    <div class="result">
+      <ul>
+        <p>{{ !keyword ? "热门文章" : "搜索结果" }}</p>
+        <ul v-if="!keyword">
+          <li
+            v-for="item of hotList"
+            :key="item.articleId"
+            @click="goToPage(item.articleId)"
+          >
+            <span class="category">@{{ item.category }}</span>
+            {{ item.title }}
+          </li>
+        </ul>
+        <el-scrollbar v-if="keyword && searchList.length" height="180px">
+          <li
+            v-for="item of searchList"
+            :key="item.articleId"
+            @click="goToPage(item.articleId)"
+          >
+            <span class="category">@{{ item.category }}</span>
+            {{ item.title }}
+          </li>
+        </el-scrollbar>
+        <el-empty
+          v-if="keyword && !searchList.length && !loading"
+          description="未搜索到相关文章"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, watchEffect } from "vue"
+import http from "@/server"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false
+  }
+})
+const emit = defineEmits(["close"])
+const keyword = ref("")
+const loading = ref(false)
+const hotList = ref<any[]>([])
+const searchList = ref<any[]>([])
+const isSearch = ref(false)
+const input = ref<HTMLInputElement>()
+
+onMounted(() => {
+  const SearchModalEL = document.querySelector("#SearchModal") as HTMLElement
+
+  SearchModalEL.addEventListener(
+    "transitionend",
+    (event) => {
+      if (event.target === SearchModalEL && !isSearch.value) {
+        emit("close")
+      }
+    },
+    false
+  )
+})
+watchEffect(() => {
+  if (props.show) {
+    keyword.value = ""
+    searchList.value = []
+    isSearch.value = true
+    input.value?.focus()
+  }
+})
+
+function close() {
+  isSearch.value = false
+}
+
+function maskClick(event: Event) {
+  const target = event.target as HTMLElement
+  if (target.getAttribute("id") === "SearchModal") {
+    close()
+  }
+}
+
+async function getHotList() {
+  const res = await http.post("/article/list", {
+    page: 1,
+    pageSize: 10,
+    orderBy: "views"
+  })
+
+  if (res.code === 0) {
+    hotList.value = res.data.list
+  } else {
+    hotList.value = []
+  }
+}
+getHotList()
+
+async function getList() {
+  try {
+    loading.value = true
+    const res = await http.get("/article/title", {
+      keyword: keyword.value
+    })
+
+    if (res.code === 0) {
+      searchList.value = res.data
+    } else {
+      searchList.value = []
+    }
+  } finally {
+    loading.value = false
+  }
+}
+
+function goToPage(articleId: string) {
+  close()
+  router.push({
+    name: "ArticleDetail",
+    params: { articleId }
+  })
+}
+</script>
+
+<style lang="scss">
+#SearchModal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  backdrop-filter: blur(2px);
+  background-color: rgba(0, 0, 0, 0.48);
+  transform: translateX(200px);
+  opacity: 0;
+  transition: all 0.5s;
+  &.enter {
+    z-index: 1000;
+    transform: translateX(0);
+    opacity: 1;
+  }
+  .input {
+    width: 600px;
+    height: 60px;
+    margin: 0 auto;
+    position: relative;
+    .el-input {
+      width: 100%;
+      height: 100%;
+      border: 0;
+      .el-input__wrapper {
+        box-shadow: initial;
+        padding: 0 40px;
+        background-color: transparent;
+        font-size: $font-size;
+        .el-input__inner {
+          color: var(--el-text-color-placeholder);
+        }
+      }
+    }
+    .searchIcon,
+    .clearIcon {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      color: var(--el-text-color-placeholder);
+      font-size: 16px;
+    }
+    .searchIcon {
+      left: 10px;
+    }
+    .clearIcon {
+      right: 10px;
+      cursor: pointer;
+      transition: all $duration;
+      &:hover {
+        color: $color-primary;
+      }
+    }
+  }
+  .result {
+    width: 600px;
+    min-height: 240px;
+    margin: 0 auto;
+    padding: 30px 0;
+    background-color: $bg-color-overlay;
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    ul {
+      width: 100%;
+      & > p {
+        padding-left: 48px;
+        color: $font-color;
+        opacity: 0.5;
+        font-size: 12px;
+        margin-bottom: 15px;
+      }
+      .el-empty {
+        padding: 0;
+      }
+      li {
+        width: 100%;
+        height: 30px;
+        line-height: 30px;
+        box-sizing: border-box;
+        padding-left: calc(48px + 1em);
+        font-size: $font-size;
+        color: $font-color;
+        transition: all $duration;
+        &:hover {
+          color: $color-primary;
+          background-color: var(--el-color-primary-light-9);
+          cursor: pointer;
+        }
+
+        .category {
+          color: #ccc;
+          margin-right: 5px;
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 850px) {
+    .input,
+    .result {
+      width: 80%;
+      ul {
+        & > p {
+          padding-left: calc($space + 10px);
+        }
+        li {
+          padding-left: calc($space + 10px + 1em);
+        }
+      }
+    }
+    .result {
+      padding: 20px 0;
+    }
+  }
+  @media screen and (max-width: 600px) {
+    .input,
+    .result {
+      width: 95%;
+    }
+  }
+}
+</style>
