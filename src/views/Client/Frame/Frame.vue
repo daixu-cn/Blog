c817f96a9b600d7059b81b4595e138fbf1a6769c
daixu <daixu.cn@outlook.com>
2023-05-17 20:48:03 +0800

    feat(pages): 客户端框架

---
+++ src/views/Client/Frame/Frame.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="Frame">
+    <Header />
+    <div id="main">
+      <router-view v-slot="{ Component }">
+        <transition name="route" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Header from "@/views/Client/Frame/Header/Header.vue"
+</script>
+
+<style lang="scss">
+#Frame {
+  #main {
+  }
+}
+</style>
