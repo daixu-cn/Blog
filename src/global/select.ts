2b0e6b6f8613f232b434707c178a70b1e04b7629
daixu <daixu.cn@outlook.com>
2023-05-17 21:00:05 +0800

    feat(global): 全局下拉框

---
+++ src/global/select.ts
@@ -0,0 +1,23 @@
+// 文章分类
+export const categories = [
+  {
+    label: "实验室",
+    value: "CODE",
+    name: "category.code"
+  },
+  {
+    label: "探索站",
+    value: "TUTORIAL",
+    name: "category.tutorial"
+  },
+  {
+    label: "咖啡屋",
+    value: "LEISURE",
+    name: "category.leisure"
+  },
+  {
+    label: "生活碎片",
+    value: "JOURNAL",
+    name: "category.journal"
+  }
+]
