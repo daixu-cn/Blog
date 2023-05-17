136c70af11c1ae7aa312e6f8255bae16f5b4de14
daixu <daixu.cn@outlook.com>
2023-05-17 20:57:26 +0800

    feat(components): 黑暗模式

---
+++ src/views/Client/Frame/Header/Mode.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="Mode" class="dropdown-container">
+    <Icon
+      v-if="themeStore.isDarkMode"
+      class="icon-dark"
+      @click="toggleThemeMode"
+    />
+    <Icon v-else class="icon-light" @click="toggleThemeMode" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import useThemeStore from "@/store/theme"
+
+const themeStore = useThemeStore()
+function toggleThemeMode() {
+  themeStore.toggleThemeMode()
+}
+</script>
