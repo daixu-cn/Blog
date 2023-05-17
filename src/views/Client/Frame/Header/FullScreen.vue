b3f216f087d9ad1967cff425cf6ed91711f9ba1f
daixu <daixu.cn@outlook.com>
2023-05-17 20:53:41 +0800

    feat(components): 全屏模块

---
+++ src/views/Client/Frame/Header/FullScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="screenfull.isEnabled" id="FullScreen" class="dropdown-container">
+    <Icon
+      v-if="!isFullscreen"
+      class="icon-fullscreen"
+      @click="screenfull.toggle()"
+    />
+    <Icon v-else class="icon-fullscreen-exit" @click="screenfull.toggle()" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue"
+import screenfull from "screenfull"
+
+const isFullscreen = ref(screenfull.isFullscreen)
+
+screenfull.onchange(() => {
+  isFullscreen.value = screenfull.isFullscreen
+})
+</script>
