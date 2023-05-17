ab9f7534435505f5e429b0199391ad39a387cd94
daixu <daixu.cn@outlook.com>
2023-05-17 20:58:10 +0800

    feat(components): icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg id="Icon" class="icon">
+    <use :xlink:href="`#${props.class}`" :fill="props.color" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  class: {
+    type: String,
+    require: true
+  },
+  color: {
+    type: String,
+    default: "currentColor"
+  },
+  size: {
+    type: String,
+    default: "1em"
+  }
+})
+</script>
+
+<style lang="scss">
+#Icon {
+  width: v-bind("props.size");
+  height: v-bind("props.size");
+  fill: v-bind("props.color");
+}
+</style>
