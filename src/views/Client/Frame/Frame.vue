3e718f46f26be4ee19de8d067a9a614c5fed4d76
daixu <daixu.cn@outlook.com>
2023-05-27 22:25:19 +0800

    chore(styles): 样式调整

--- src/views/Client/Frame/Frame.vue
+++ src/views/Client/Frame/Frame.vue
@@ -8,6 +8,7 @@
         </transition>
       </router-view>
     </div>
+    <el-backtop :right="30" :bottom="30" :visibility-height="2000" />
   </div>
 </template>
 
@@ -18,6 +19,17 @@ import Header from "@/views/Client/Frame/Header/Header.vue"
 <style lang="scss">
 #Frame {
   #main {
+    width: $main-width;
+    min-height: calc(100vh - 60px);
+    padding-top: 20px;
+    box-sizing: border-box;
+    margin: 0 auto;
+  }
+  @media screen and (max-width: 1250px) {
+    #main {
+      width: 100%;
+      padding: 20px 20px 0 20px;
+    }
   }
 }
 </style>
