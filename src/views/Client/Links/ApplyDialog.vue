2cd7cf9a70d62ce8643d4f6c19c9d05138206627
daixu <daixu.cn@outlook.com>
2023-05-28 13:44:43 +0800

    feat(pages): 友情链接模块

---
+++ src/views/Client/Links/ApplyDialog.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-dialog
+    id="ApplyDialog"
+    v-model="show"
+    title="友联申请"
+    @close="linkRef?.resetFields()"
+  >
+    <el-form
+      ref="linkRef"
+      :model="link"
+      :rules="rules"
+      status-icon
+      label-width="0"
+    >
+      <el-form-item prop="name">
+        <el-input v-model="link.name" placeholder="网站名称:" />
+      </el-form-item>
+      <el-form-item prop="url">
+        <el-input v-model="link.url" placeholder="网站地址:" />
+      </el-form-item>
+      <el-form-item prop="description">
+        <el-input v-model="link.description" placeholder="网站描述:" />
+      </el-form-item>
+      <el-form-item prop="logo">
+        <el-input v-model="link.logo" placeholder="网站LOGO:" />
+      </el-form-item>
+      <el-form-item prop="email">
+        <el-input v-model="link.email" placeholder="站长邮箱:" />
+      </el-form-item>
+      <el-form-item prop="qq">
+        <el-input v-model="link.qq" placeholder="网站QQ:" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button type="primary" :loading="loading" @click="confirm">
+          提交申请
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { ref, reactive } from "vue"
+import http from "@/server"
+import { ElMessage } from "element-plus"
+import type { FormInstance, FormRules } from "element-plus"
+
+const show = ref(false)
+const loading = ref(false)
+const link = reactive({
+  name: null,
+  description: null,
+  logo: null,
+  url: null,
+  email: null,
+  qq: null
+})
+const linkRef = ref<FormInstance>()
+const rules = reactive<FormRules>({
+  name: [{ required: true, message: "网站名称不能为空", trigger: "blur" }],
+  url: [{ required: true, message: "网站地址不能为空", trigger: "blur" }]
+})
+
+async function confirm() {
+  const valid = await linkRef.value?.validate()
+  if (valid) {
+    loading.value = true
+    const res = await http.put("/link/create", link)
+    if (res.code === 0) {
+      ElMessage.success("申请成功,等待审核...")
+      show.value = false
+    }
+    loading.value = false
+  }
+}
+
+defineExpose({
+  show
+})
+</script>
+
+<style lang="scss">
+#ApplyDialog {
+  .el-dialog__body {
+    padding-bottom: 0;
+    .el-input__wrapper {
+      box-shadow: none;
+      border-bottom: 1px solid $border-color;
+    }
+    .el-form-item__error {
+      padding-left: 11px;
+    }
+  }
+  .dialog-footer {
+    width: 100%;
+    .el-button {
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/views/Client/Links/Links.vue
+++ src/views/Client/Links/Links.vue
@@ -1,10 +1,226 @@
 <template>
-  <div id="Links">友情链接</div>
+  <div id="Links">
+    <el-skeleton :loading="skeletonLoading && page === 1" animated>
+      <template #template>
+        <div class="link-container">
+          <div v-for="item of 10" :key="item" class="link-item">
+            <el-skeleton-item variant="image" class="logo" />
+            <div class="info">
+              <el-skeleton-item variant="text" class="name" />
+              <el-skeleton-item variant="h1" class="description" />
+            </div>
+          </div>
+          <i v-for="item of 4" :key="item" class="placeholder" />
+        </div>
+      </template>
+      <template #default>
+        <div class="link-container">
+          <a
+            v-for="item in list"
+            :key="item.linkId"
+            class="link-item"
+            target="_blank"
+            :href="item.url"
+          >
+            <el-image class="logo" :src="item.logo ?? ''" fit="contain">
+              <template #error>
+                <div class="image-slot">
+                  <i-ep-picture />
+                </div>
+              </template>
+            </el-image>
+
+            <div class="info">
+              <p class="name">{{ item.name }}</p>
+              <div class="description">{{ item.description }}</div>
+            </div>
+          </a>
+          <a class="link-item apply" @click="applyDialog.show = true">
+            <i-ep-plus />友联申请
+          </a>
+
+          <i v-for="item of 4" :key="item" class="placeholder" />
+        </div>
+      </template>
+    </el-skeleton>
+
+    <div ref="footer" class="footer">
+      <Loading :loading="loading" />
+    </div>
+    <ApplyDialog ref="applyDialog" />
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive, ref, nextTick } from "vue"
+import http from "@/server"
+import dayjs from "dayjs"
+import { useIntersectionObserver } from "@vueuse/core"
+import Loading from "@/components/Loading.vue"
+import ApplyDialog from "./ApplyDialog.vue"
+
+const footer = ref()
+const applyDialog = ref()
+const list = reactive<any[]>([])
+const skeletonLoading = ref(true)
+const loading = ref(false)
+const page = ref(1)
+const total = ref(0)
+
+async function getList() {
+  try {
+    if (page.value === 1) {
+      skeletonLoading.value = true
+    } else {
+      loading.value = true
+    }
+    const res = await http.post("/link/list", {
+      page: page.value,
+      pageSize: 20
+    })
+
+    if (res.code === 0) {
+      list.push(
+        ...res.data.list.map((item) => {
+          return {
+            ...item,
+            createdAt: dayjs(item.createdAt).format("L LTS"),
+            updatedAt: dayjs(item.updatedAt).format("L LTS")
+          }
+        })
+      )
+      total.value = res.data.total
+
+      nextTick(() => {
+        const { stop } = useIntersectionObserver(
+          footer,
+          ([{ isIntersecting }]) => {
+            stop()
+            if (isIntersecting) {
+              if (list.length < total.value) {
+                getList()
+              }
+            }
+          }
+        )
+      })
+    }
+  } finally {
+    if (page.value === 1) {
+      skeletonLoading.value = false
+    } else {
+      loading.value = false
+    }
+    if (list.length >= total.value) {
+      stop()
+    }
+    page.value++
+  }
+}
+getList()
+
+const { stop } = useIntersectionObserver(footer, ([{ isIntersecting }]) => {
+  if (isIntersecting) {
+    if (list.length < total.value) {
+      getList()
+    }
+  }
+})
+</script>
 
 <style lang="scss">
 #Links {
+  .link-container {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .link-item {
+      width: calc(25% - 15px);
+      padding: 10px;
+      box-sizing: border-box;
+      margin-bottom: 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      cursor: pointer;
+      border-radius: $border-radius;
+      box-shadow: var(--el-box-shadow);
+      text-decoration: none;
+      .logo {
+        width: 70px;
+        height: 70px;
+        .image-slot {
+          width: 100%;
+          height: 100%;
+          @include flex-center;
+        }
+      }
+      .info {
+        width: calc(100% - 80px);
+        user-select: none;
+        transition: color $duration;
+        .name {
+          color: $font-color;
+          @include text-ellipsis;
+        }
+        .description {
+          color: $font-color-secondary;
+          margin-top: 10px;
+          @include more-text-ellipsis(2);
+        }
+      }
+      &:hover {
+        .info {
+          .name,
+          .description {
+            color: $color-primary;
+          }
+        }
+      }
+      &.apply {
+        color: $color-primary;
+        justify-content: center;
+        font-size: 1.1em;
+        min-height: 80px;
+        svg {
+          margin-right: 5px;
+        }
+      }
+    }
+    .placeholder {
+      width: calc(25% - 15px);
+      height: 0;
+    }
+  }
+
+  .footer {
+    margin-bottom: 20px;
+  }
+
+  @media screen and (max-width: 1000px) {
+    .link-container {
+      .link-item,
+      .placeholder {
+        width: calc(100% / 3 - 20px);
+      }
+    }
+  }
+  @media screen and (max-width: 800px) {
+    .link-container {
+      .link-item,
+      .placeholder {
+        width: calc(50% - 10px);
+      }
+    }
+  }
+  @media screen and (max-width: 550px) {
+    .link-container {
+      .link-item,
+      .placeholder {
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
