8b6cc6f307103ee61a86354c110585502aea08bb
daixu <daixu.cn@outlook.com>
2023-05-29 22:19:36 +0800

    chore(styles): z-index层级调整

--- src/views/Client/Frame/Header/Header.vue
+++ src/views/Client/Frame/Header/Header.vue
@@ -50,7 +50,7 @@ function goToPage(name: string) {
 #Header {
   position: sticky;
   top: 0;
-  z-index: 1;
+  z-index: 3;
   height: 60px;
   box-sizing: border-box;
   border-bottom: 1px solid $border-color;
