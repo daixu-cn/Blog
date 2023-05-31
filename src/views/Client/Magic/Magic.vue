a7387747282fe72894eacabae1a0de94a91078e8
daixu <daixu.cn@outlook.com>
2023-05-31 19:45:01 +0800

    chore: 占位

--- src/views/Client/Magic/Magic.vue
+++ src/views/Client/Magic/Magic.vue
@@ -1,10 +1,14 @@
 <template>
-  <div id="Magic">魔法空间</div>
+  <div id="Magic">没想好啥整啥～</div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="scss">
 #Magic {
+  width: 100%;
+  height: calc(100vh - 80px);
+  font-size: 2em;
+  @include flex-center;
 }
 </style>
