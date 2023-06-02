31534665dfb8b9558cfdb335f2ca0b8ad1e25d67
daixu <daixu.cn@outlook.com>
2023-06-02 16:34:48 +0800

    refactor: 抽离第三方登录跳转地址

--- src/global/env.ts
+++ src/global/env.ts
@@ -4,3 +4,15 @@ export const BASE_API = import.meta.env.VITE_APP_BASE_API
 export const DEFAULE_LOCALE = import.meta.env.VITE_APP_DEFAULE_LOCALE
 // 默认主题
 export const DEFAULE_THEME = import.meta.env.VITE_APP_DEFAULE_THEME
+
+// 第三方授权重定向地址
+const redirectURI = "https://daixu.cn/oauth"
+// 跳转到授权地址
+export function OAuth(type: "qq" | "github" | "google") {
+  if (type === "qq") {
+    window.location.href = `https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=101959622&redirect_uri=${redirectURI}&state=qq`
+  }
+  if (type === "github") {
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=52a68a030ce19089d0f1&redirect_uri=${redirectURI}&state=github`
+  }
+}
