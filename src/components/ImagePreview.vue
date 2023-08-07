1c81f5d6c8cbfaeedc980a9a1997d45e8b6b5c1c
daixu <daixu.cn@outlook.com>
2023-08-07 09:58:53 +0800

    feat(components): 图片预览

---
+++ src/components/ImagePreview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-image-viewer
+      v-if="props.show"
+      :url-list="props.urlList"
+      teleported
+      @close="emits('close')"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from "vue"
+
+const props = defineProps({
+  show: {
+    type: Boolean,
+    default: false
+  },
+  urlList: {
+    type: Array as PropType<string[]>,
+    default() {
+      return []
+    }
+  }
+})
+const emits = defineEmits(["close"])
+</script>
