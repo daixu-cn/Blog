135cde35bcf41891293255d2a46c46e6a8514b12
daixu <daixu.cn@outlook.com>
2023-08-15 16:59:03 +0800

    refactor: 第三方授权登录信息抽离

--- src/global/env.ts
+++ src/global/env.ts
@@ -4,17 +4,23 @@ export const BASE_API = import.meta.env.VITE_APP_BASE_API
 export const DEFAULE_THEME = import.meta.env.VITE_APP_DEFAULE_THEME
 
 // 第三方授权重定向地址
-const redirectURI = "https://daixu.cn/oauth"
+const redirectURI = import.meta.env.VITE_APP_OAUTH_REDIRECT
 // 跳转到授权地址
 export function OAuth(type: "qq" | "github" | "google") {
   if (type === "qq") {
-    window.location.href = `https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=101959622&redirect_uri=${redirectURI}&state=qq`
+    window.location.href = `https://graph.qq.com/oauth2.0/authorize?response_type=token&client_id=${
+      import.meta.env.VITE_APP_OAUTH_QQ_CLIENT_ID
+    }&redirect_uri=${redirectURI}&state=qq`
   }
   if (type === "github") {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=52a68a030ce19089d0f1&redirect_uri=${redirectURI}&state=github`
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${
+      import.meta.env.VITE_APP_OAUTH_GITHUB_CLIENT_ID
+    }&redirect_uri=${redirectURI}&state=github`
   }
   if (type === "google") {
-    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=149094609811-jt66sud8u9qmvueehdh8rhani1aqaeiv.apps.googleusercontent.com&redirect_uri=${redirectURI}&scope=${encodeURIComponent(
+    window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+      import.meta.env.VITE_APP_OAUTH_GOOGLE_CLIENT_ID
+    }&redirect_uri=${redirectURI}&scope=${encodeURIComponent(
       "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid"
     )}&response_type=token&state=google`
   }
