fe49fc7cc8c28656feddb2fa52df0a843a741380
daixu <daixu.cn@outlook.com>
2023-08-15 11:12:19 +0800

    chore: 添加注释

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,3 +1,4 @@
+// 主题文件
 export type ThemeType = "clear-spring-green" | "ethereal-orchid-purple"
 
 interface Theme {
@@ -5,11 +6,14 @@ interface Theme {
   theme: ThemeType
   color: string
 }
-
+// 主题数组
 const themes: Theme[] = [
   {
+    // 颜色中文标题
     title: "清泉翠",
+    // 主题的文件名
     theme: "clear-spring-green",
+    // 主题颜色
     color: "#45d6bb"
   },
   {
