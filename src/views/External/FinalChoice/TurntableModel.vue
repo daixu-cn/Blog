363c9f0cb71c0ecda8a007c7d46cbf0e140639c6
daixu <daixu.cn@outlook.com>
2023-10-16 14:05:26 +0800

    feat(pages): 优化转盘运行动画

--- src/views/External/FinalChoice/TurntableModel.vue
+++ src/views/External/FinalChoice/TurntableModel.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="External-FinalChoice-TurntableModel" @click="confirmToggle">
+  <div id="External-FinalChoice-TurntableModel" @click="start">
     <canvas class="turntable" />
     <div class="arrow"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { PropType, onDeactivated, onMounted, watch, nextTick } from "vue"
+import { PropType, watch, nextTick } from "vue"
 import tinycolor from "tinycolor2"
 import useThemeStore from "@/store/theme"
+import { getRandomIntInclusive } from "@/utils/util"
 import { TableData } from "./types"
 
 const themeStore = useThemeStore()
@@ -18,7 +19,7 @@ const props = defineProps({
     default: () => []
   }
 })
-let animation: Animation
+let animation: Animation | undefined
 
 function drawCarousel() {
   const html = document.querySelector("html") as HTMLElement
@@ -32,10 +33,15 @@ function drawCarousel() {
   ) as HTMLCanvasElement
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
+  // 画布大小
   const squareSize = 300
+  // 角度转弧度
   const deg = Math.PI / 180
+  // 半径
   const radius = squareSize / 2
+  // 每个扇形的角度
   const angle = (100 / props.optionsList.length / 100) * 360
+  // 起始角度
   let startAngle = 45
 
   canvas.width = squareSize
@@ -62,9 +68,10 @@ function drawCarousel() {
     ctx.lineTo(squareSize / 2, squareSize / 2)
     // 填充扇形区域
     ctx.fill()
+    // 保存画布状态
+    ctx.save()
 
     // 添加文本
-    ctx.save()
     ctx.fillStyle = "#fff"
     ctx.font = `${fontSize} ${fontFamily}`
     ctx.translate(squareSize / 2, squareSize / 2)
@@ -74,49 +81,62 @@ function drawCarousel() {
       -ctx.measureText(props.optionsList[i].option).width / 2,
       -radius / 2
     )
+
+    // 恢复画布状态
     ctx.restore()
 
     startAngle += angle
   }
 }
 
-function stop() {
-  animation.pause()
-}
+function start() {
+  if (animation?.playState !== "running") {
+    const arrow = document.querySelector(
+      "#External-FinalChoice-TurntableModel .arrow"
+    )
+    const turntable = document.querySelector(
+      "#External-FinalChoice-TurntableModel .turntable"
+    )
 
-function confirmToggle() {
-  if (animation.playState === "running") {
-    stop()
-  } else {
-    animation.play()
+    // 旋转圈数
+    const turns = getRandomIntInclusive(360 * 15, 360 * 20)
+    // 结束时旋转圈数
+    const overTurns = turns + getRandomIntInclusive(0, 360 * 2)
+    // 动画配置
+    const options: AnimationEffectTiming = {
+      easing: "cubic-bezier(.47,.17,0,.99)",
+      duration: 1000 * 5,
+      fill: "forwards"
+    }
+
+    animation = arrow?.animate(
+      [
+        { transform: "translate(-50%, -95%) rotate(0deg)" },
+        {
+          transform: `translate(-50%, -95%) rotate(${overTurns}deg)`
+        }
+      ],
+      options
+    )
+    turntable?.animate(
+      [
+        { transform: "rotate(0deg)" },
+        {
+          transform: `rotate(-${overTurns}deg)`
+        }
+      ],
+      options
+    )
   }
 }
 
-onDeactivated(stop)
-
-onMounted(() => {
-  const turntable = document.querySelector(
-    "#External-FinalChoice-TurntableModel .turntable"
-  ) as HTMLCanvasElement
-  animation = turntable.animate(
-    [
-      {
-        transform: "rotate(0deg)"
-      },
-      {
-        transform: "rotate(360deg)"
-      }
-    ],
-    {
-      duration: 30,
-      iterations: Infinity
-    }
-  )
-  animation.pause()
-
-  drawCarousel()
-})
-watch(() => props.optionsList, drawCarousel)
+watch(
+  () => props.optionsList,
+  () => {
+    nextTick(drawCarousel)
+  },
+  { immediate: true }
+)
 watch(
   () => themeStore.current,
   () => {
@@ -138,6 +158,7 @@ watch(
     top: 50%;
     left: 50%;
     transform: translate(-50%, -95%);
+    transform-origin: bottom center;
     &::before {
       content: "";
       display: block;
