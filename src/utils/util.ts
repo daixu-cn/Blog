a074ea0ae68429fe52a459dba503e1d39e274021
daixu <daixu.cn@outlook.com>
2023-10-16 13:56:39 +0800

    chore(utils): 生成随机数

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -7,3 +7,25 @@
 export const getAssets = (fileName: string, folder = "image") => {
   return new URL(`../assets/${folder}/${fileName}`, import.meta.url).href
 }
+
+/**
+ * @description 获取两个数之间的随机整数（包含最大值和最小值）
+ * @param {number} min 最小值
+ * @param {number} max 最大值
+ * @return {number}
+ */
+export const getRandomIntInclusive = (min: number, max: number): number => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+/**
+ * @description 获取两个数之间的随机数【min<= r < max】
+ * @param {number} min 最小值
+ * @param {number} max 最大值
+ * @return {number}
+ */
+function getRandomDouble(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
