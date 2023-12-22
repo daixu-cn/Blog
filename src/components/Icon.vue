6038de25ef57bd9419192841eae21d46285d33e9
daixu <daixu.cn@outlook.com>
2023-12-22 11:06:57 +0800

    feat: 增加SVG描边动画配置

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,10 +1,16 @@
 <template>
-  <svg id="Icon" :class="props.class">
+  <svg id="Icon" :class="[props.class, identifier]">
     <use :xlink:href="`#${props.class}`" :fill="props.color" />
   </svg>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, computed } from "vue"
+import { nanoid } from "nanoid"
+
+const identifier = computed(() => {
+  return `Icon-${nanoid()}`
+})
 const props = defineProps({
   class: {
     type: String,
@@ -17,6 +23,54 @@ const props = defineProps({
   size: {
     type: String,
     default: "1em"
+  },
+  // 描边动画颜色(如果不传表示不开启动画)
+  stroke: {
+    type: String
+  },
+  // 描边宽度
+  strokeWidth: {
+    type: String,
+    default: "10px"
+  }
+})
+
+onMounted(() => {
+  if (props.stroke) {
+    const icon = document.querySelector<SVGSymbolElement>(
+      `.${identifier.value}`
+    )
+    const use = document.querySelector<SVGUseElement>(
+      `.${identifier.value} use`
+    )
+    const href = use?.getAttribute("xlink:href")
+    if (href) {
+      // 找到对应的图标容器
+      const container = document.querySelector(href)
+      const paths = container?.querySelectorAll<SVGPathElement>("path") ?? []
+
+      for (const path of paths) {
+        const length = Math.ceil(path.getTotalLength()).toString()
+        path.setAttribute("length", length)
+        path.style.strokeDasharray = length
+        path.style.strokeDashoffset = length
+        path.style.stroke = props.stroke
+        path.style.strokeWidth = props.strokeWidth
+      }
+
+      icon?.addEventListener("mouseenter", () => {
+        for (const path of paths) {
+          path.style.strokeDashoffset = "0"
+        }
+      })
+      icon?.addEventListener("mouseleave", () => {
+        for (const path of paths) {
+          const length = path.getAttribute("length") ?? "0"
+          path.style.strokeDasharray = length
+          path.style.strokeDashoffset = length
+        }
+      })
+    }
   }
 })
 </script>
@@ -26,5 +80,9 @@ const props = defineProps({
   width: v-bind("props.size");
   height: v-bind("props.size");
   fill: v-bind("props.color");
+}
+
+svg symbol[id^="icon-"] path {
+  transition: stroke-dashoffset 1s;
 }
 </style>
