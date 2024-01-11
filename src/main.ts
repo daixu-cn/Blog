bde02837552002889e84064fd729949121708918
daixu <daixu.cn@outlook.com>
2024-01-11 15:13:26 +0800

    refactor: 移除手动注册组件

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import app from "@/global/app"
 import "@/assets/style/scss/global.scss"
 import "@/plugins"
 import "@/directives"
-import "@/components"
 
 import "web-animations-js"
 import "@/global/iconfont"
