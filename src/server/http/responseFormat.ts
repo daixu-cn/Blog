6fa03b31c62ed141effbe14d06c2fe5c5f101904
daixu <daixu.cn@outlook.com>
2024-02-05 09:19:13 +0800

    fix: 异常登录重置用户状态

--- src/server/http/responseFormat.ts
+++ src/server/http/responseFormat.ts
@@ -1,20 +1,21 @@
 import { AxiosResponse } from "axios"
 import { ElMessage } from "element-plus"
+import useUserStore from "@/store/user"
 
 export default function (response: AxiosResponse) {
   const code = response.data?.code
 
   if (code !== 0) {
-    if (code === 401 || code === 403) {
-      localStorage.removeItem("token")
-    }
-
     if (code === 12009) {
       ElMessage.warning("已获取上次中断文件，继续上传中...")
       return response.data
     }
     if (code === 12011) {
       return response.data
+    }
+
+    if (code === 401 || code === 403) {
+      useUserStore()?.reset()
     }
 
     ElMessage.error(response.data.message)
